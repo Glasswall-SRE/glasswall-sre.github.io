c5bda925e9e62c411a30853e5d73258924579d7d
Alex Potter-Dixon <apotter-dixon@glasswallsolutions.com>
2020-02-18 17:42:52 +0000

    initial commit

---
+++ docusaurus.config.js
@@ -0,0 +1,91 @@
+***REMOVED***
+  title: 'Glasswall SRE',
+  tagline: 'Helping create scalable and reliable products at Glasswall!',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  favicon: 'img/favicon.ico',
+  organizationName: 'Glasswall', // Usually your GitHub org/user name.
+  projectName: 'SRE', // Usually your repo name.
+***REMOVED***
+***REMOVED***
+      title: 'Glasswall SRE',
+***REMOVED***
+        alt: 'My Site Logo',
+        src: 'img/logo.svg',
+  ***REMOVED***
+***REMOVED***
+***REMOVED*** to: 'docs/doc1', label: 'Docs', position: 'left' },
+***REMOVED*** to: 'blog', label: 'Blog', position: 'left' },
+***REMOVED***
+          href: 'https://github.com/facebook/docusaurus',
+          label: 'GitHub',
+          position: 'right',
+    ***REMOVED***
+***REMOVED***
+***REMOVED***
+    footer: {
+      style: 'dark',
+***REMOVED***
+***REMOVED***
+          title: 'Docs',
+          items: [
+    ***REMOVED***
+              label: 'Style Guide',
+              to: 'docs/doc1',
+        ***REMOVED***
+    ***REMOVED***
+              label: 'Second Doc',
+              to: 'docs/doc2',
+        ***REMOVED***
+    ***REMOVED***
+    ***REMOVED***
+***REMOVED***
+          title: 'Community',
+          items: [
+    ***REMOVED***
+              label: 'Stack Overflow',
+              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+        ***REMOVED***
+    ***REMOVED***
+              label: 'Discord',
+              href: 'https://discordapp.com/invite/docusaurus',
+        ***REMOVED***
+    ***REMOVED***
+    ***REMOVED***
+***REMOVED***
+          title: 'Social',
+          items: [
+    ***REMOVED***
+              label: 'Blog',
+              to: 'blog',
+        ***REMOVED***
+    ***REMOVED***
+              label: 'GitHub',
+              href: 'https://github.com/facebook/docusaurus',
+        ***REMOVED***
+    ***REMOVED***
+              label: 'Twitter',
+              href: 'https://twitter.com/docusaurus',
+        ***REMOVED***
+    ***REMOVED***
+    ***REMOVED***
+***REMOVED***
+      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+      '@docusaurus/preset-classic',
+***REMOVED***
+***REMOVED***
+          sidebarPath: require.resolve('./sidebars.js'),
+***REMOVED***
+            'https://github.com/facebook/docusaurus/edit/master/website/',
+    ***REMOVED***
+***REMOVED***
+          customCss: require.resolve('./src/css/custom.css'),
+    ***REMOVED***
+  ***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
