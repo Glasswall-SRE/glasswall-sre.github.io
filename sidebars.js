798661d86d1f08085a07dcffd9a84242d523d420
Alex Potter-Dixon <apotter-dixon@glasswallsolutions.com>
2020-03-24 11:22:37 +0000

    Further Filetrust email migration

--- sidebars.js
+++ sidebars.js
@@ -1,28 +1,31 @@
 ***REMOVED***
   docs: [
     {
-      type: 'category',
-      label: 'About',
-      items: [
-        "about/what-is-sre",
-        "about/sre-concepts"
-      ]
+      type: "category",
+      label: "About",
+      items: ["about/what-is-sre", "about/sre-concepts"]
 ***REMOVED***
     {
-      type: 'category',
-      label: 'Products',
+      type: "category",
+      label: "Products",
       items: [
 ***REMOVED***
-          type: 'category',
-          label: 'FileTrust Email',
+          type: "category",
+          label: "FileTrust Email",
           items: [
             "products/filetrust-email/introduction",
             "products/filetrust-email/slos",
-            "products/filetrust-email/data-management"
+            "products/filetrust-email/data-management",
+            "products/filetrust-email/releases",
+            "products/filetrust-email/logging-retention"
           ]
-    ***REMOVED***
-
+        }
       ]
+***REMOVED***
+    {
+      type: "category",
+      label: "Standards",
+      items: ["standards/software"]
     }
   ]
 ***REMOVED***
