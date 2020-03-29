4768dc515551550e8fd781a61a98dbb419064638
Alex Potter-Dixon <apotter-dixon@glasswallsolutions.com>
2020-03-29 13:17:09 +0100

    Working tech radar

--- src/pages/supported-tech.js
+++ src/pages/supported-tech.js
@@ -8,7 +8,7 @@ function TechRadar() {
         height="100%"
         width="100%"
         sandbox="allow-forms allow-popups allow-pointer-lock allow-same-origin allow-scripts"
-        src="https://radar.thoughtworks.com/?sheetId=https%3A%2F%2Fdocs.google.com%2Fspreadsheets%2Fd%2F1nSSkvaZMZfXdwkNUNohrl3NYshppB6f-YURVFI5Uhu4%2Fedit%23gid%3D1985253373"
+        src="https://radar.thoughtworks.com/?sheetId=https%3A%2F%2Fdocs.google.com%2Fspreadsheets%2Fd%2F1dyoTqbyLWopznuJUl6ciBptujuEkEJQeVIiAq0pyk80%2Fedit%23gid%3D0"
       ></iframe>
       ;
     </Layout>
