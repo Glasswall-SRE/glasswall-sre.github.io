8430ebb1020ca48346b58c3e440467bbd2c5ceb6
Alex Potter-Dixon <apotter-dixon@glasswallsolutions.com>
2020-04-27 09:32:35 +0100

    API key replaced

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,51 +8,51 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
-      apiKey: "064acddd10c4e945f87f7fdfaab7579f",
 ***REMOVED***
-      algoliaOptions: {} // Optional, if provided by Algolia
+***REMOVED***
+***REMOVED***
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
     ***REMOVED***
 ***REMOVED***
 ***REMOVED***
-        src: "img/logo.svg"
+***REMOVED***
   ***REMOVED***
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
-          position: "left"
+***REMOVED***
     ***REMOVED***
 ***REMOVED*** to: "supported-tech", label: "Supported Tech", position: "left" },
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
-          position: "right"
+***REMOVED***
     ***REMOVED***
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
-          position: "right"
-        }
-      ]
-    }
 ***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-            "https://dev.azure.com/glasswall/Glasswall%20Cloud/_git/Glasswall.SRE.Documentation?path=%2F"
     ***REMOVED***
 ***REMOVED***
-          customCss: require.resolve("./src/css/custom.css")
-        }
-      }
-    ]
-  ]
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+    ***REMOVED***
+***REMOVED***
+***REMOVED***
+    ***REMOVED***
+  ***REMOVED***
+***REMOVED***
+***REMOVED***
 ***REMOVED***
