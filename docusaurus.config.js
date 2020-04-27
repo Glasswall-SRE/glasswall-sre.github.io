033ac4dbb9ba45d0b35ce710cf62501c112fb343
Alex Potter-Dixon <apotter-dixon@glasswallsolutions.com>
2020-04-27 09:32:35 +0100

    API key replaced

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,37 +8,37 @@ module.exports = {
   projectName: "SRE", // Usually your repo name.
   themeConfig: {
     algolia: {
-      apiKey: "064acddd10c4e945f87f7fdfaab7579f",
+      apiKey: "#{sre-site-algolia-api-key}#",
       indexName: "glasswallsolutions_sre",
-      algoliaOptions: {} // Optional, if provided by Algolia
+      algoliaOptions: {}, // Optional, if provided by Algolia
     },
     disableDarkMode: true,
     navbar: {
       title: "Glasswall SRE",
       logo: {
         alt: "My Site Logo",
-        src: "img/logo.svg"
+        src: "img/logo.svg",
       },
       links: [
         { to: "docs/sre/what-is-sre", label: "Docs", position: "left" },
         {
           to: "docs/products/filetrust-email/introduction",
           label: "Products",
-          position: "left"
+          position: "left",
         },
         { to: "supported-tech", label: "Supported Tech", position: "left" },
         {
           href: "https://medium.com/glasswall-engineering",
           label: "Medium",
-          position: "right"
+          position: "right",
         },
         {
           href: "https://github.com/Glasswall-SRE",
           label: "GitHub",
-          position: "right"
-        }
-      ]
-    }
+          position: "right",
+        },
+      ],
+    },
   },
   presets: [
     [
@@ -47,12 +47,12 @@ module.exports = {
         docs: {
           sidebarPath: require.resolve("./sidebars.js"),
           editUrl:
-            "https://dev.azure.com/glasswall/Glasswall%20Cloud/_git/Glasswall.SRE.Documentation?path=%2F"
+            "https://dev.azure.com/glasswall/Glasswall%20Cloud/_git/Glasswall.SRE.Documentation?path=%2F",
         },
         theme: {
-          customCss: require.resolve("./src/css/custom.css")
-        }
-      }
-    ]
-  ]
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      },
+    ],
+  ],
 };
